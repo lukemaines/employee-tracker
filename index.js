0c087752bc2e2f719046b6a645e8d66e749b90ed
l.maines <lmmaines28@gmail.com>
2024-08-27 22:54:21 -0400

    created connection and index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const client = require('./db/connection');
+
+client.query('SELECT * FROM employee', (err, res) => {
+    if (err) {
+        console.error('Error executing query', err);
+    } else {
+        console.log(res);
+    }
+    client.end();
+})
